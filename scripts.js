1e9bc54c46c07acf7a8f32940fb4ed5341effe5e
Johvahn <johvahnbro@gmail.com>
2021-10-14 11:46:52 -0500

    Edited files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+window.onload = function() {
+    var btn = document.querySelector("button");
+    var m = document.getElementsbyClassName("message");
+    btn.addEventListener("click", function(event) {
+        event.preventDefault();
+        var t = document.getElementbyId("email").value;
+        if (t != "") {
+            m[0].innerHTML = "Thank you! Your email address " + t + " has been added to our mailing list!";
+        } 
+        else {
+            m[0].innerHTML = "Please enter a valid email address";
+        }
+    });
+}
